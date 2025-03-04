cab1daf6f7609f0ec28c7f17c33a75311d6c27e6
Vjekoslav-Radosevic <vjekoslav.radosevic@gmail.com>
2025-03-04 23:21:28 +0100

    add Weak entity shape

---
+++ src/erDiagram/shapes/entities/WeakEntityShape.js
@@ -0,0 +1,24 @@
+import { Shape } from "../Shape.js";
+
+export class WeakEntityShape extends Shape {
+    constructor(ctx, x, y, width, height) {
+        super(ctx, x, y, width, height);
+    }
+
+    // Check if the mouse is inside the rectangle
+    isInside(mouseX, mouseY) {
+        return mouseX >= this.x && mouseX <= this.x + this.width && mouseY >= this.y && mouseY <= this.y + this.height;
+    }
+
+    draw(text, backgroundColor, textColor) {
+        this.ctx.fillStyle = backgroundColor;
+        this.ctx.fillRect(this.x, this.y, this.width, this.height); // Draw the rectangle
+
+        this.ctx.strokeRect(this.x, this.y, this.width, this.height); // Draw the outer border
+        this.ctx.strokeRect(this.x + 5, this.y + 5, this.width - 10, this.height - 10); // Draw the inner border
+
+        // Draw the name inside the rectangle
+        this.ctx.fillStyle = textColor;
+        this.ctx.fillText(text, this.x + this.width / 2, this.y + this.height / 2);
+    }
+}
