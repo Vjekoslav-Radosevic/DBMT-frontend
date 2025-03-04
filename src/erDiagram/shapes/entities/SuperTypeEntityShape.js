88064626cac06a6bb01202c12c0815cf1968244b
Vjekoslav-Radosevic <vjekoslav.radosevic@gmail.com>
2025-03-04 23:21:40 +0100

    add SuperType entity shape

---
+++ src/erDiagram/shapes/entities/SuperTypeEntityShape.js
@@ -0,0 +1,73 @@
+import { Shape } from "../Shape.js";
+
+export class SuperTypeEntityShape extends Shape {
+    constructor(ctx, x, y, width, height) {
+        super(ctx, x, y, width, height);
+
+        this.circleRadius = 15;
+        this.entityToCircleDistance = 20;
+    }
+
+    // Check if the mouse is inside the rectangle
+    isInside(mouseX, mouseY) {
+        return mouseX >= this.x && mouseX <= this.x + this.width && mouseY >= this.y && mouseY <= this.y + this.height;
+    }
+
+    getCircleCenter() {
+        const centerX = this.x + this.width / 2;
+        const centerY = this.y + this.height + this.circleRadius + this.entityToCircleDistance;
+        return { x: centerX, y: centerY };
+    }
+
+    drag(mouseX, mouseY, entities, attributes) {
+        if (this.isDragging) {
+            const deltaX = mouseX - this.offset.x - this.x;
+            const deltaY = mouseY - this.offset.y - this.y;
+
+            // Update the entity's position
+            this.x = mouseX - this.offset.x;
+            this.y = mouseY - this.offset.y;
+
+            entities.forEach((entity) => {
+                entity.updatePosition(deltaX, deltaY);
+            });
+
+            attributes.forEach((attribute) => {
+                attribute.updatePosition(deltaX, deltaY);
+            });
+        }
+    }
+
+    draw(name, type, backgroundColor, textColor) {
+        this.ctx.fillStyle = backgroundColor;
+        this.ctx.fillRect(this.x, this.y, this.width, this.height); // Draw the rectangle
+
+        this.ctx.strokeRect(this.x, this.y, this.width, this.height); // Draw the border
+
+        // Draw the name inside the rectangle
+        this.ctx.fillStyle = textColor;
+        this.ctx.fillText(name, this.x + this.width / 2, this.y + this.height / 2);
+
+        // Draw the circle beneath the rectangle
+        const circleX = this.x + this.width / 2; // Center of the circle
+        const circleY = this.y + this.height + this.circleRadius + 20; // Slightly below the rectangle
+
+        // Draw the line from the bottom of the rectangle to the top of the circle
+        this.ctx.beginPath();
+        this.ctx.moveTo(circleX, this.y + this.height); // Bottom center of the rectangle
+        this.ctx.lineTo(circleX, circleY - this.circleRadius); // Top center of the circle
+        this.ctx.closePath();
+        this.ctx.stroke();
+
+        //Draw the circle
+        this.ctx.beginPath();
+        this.ctx.arc(circleX, circleY, this.circleRadius, 0, Math.PI * 2, false); // Draw the circle
+        this.ctx.fillStyle = backgroundColor; // Same color as the rectangle background
+        this.ctx.closePath();
+        this.ctx.fill(); // Fill the circle
+        this.ctx.stroke(); // Draw the border of the circle
+
+        this.ctx.fillStyle = textColor;
+        this.ctx.fillText(type, circleX, circleY);
+    }
+}
