860b34cc129721fac3a39cb50f765a7669a82ccb
Vjekoslav-Radosevic <vjekoslav.radosevic@gmail.com>
2025-03-04 23:20:58 +0100

    add Associative entity shape

---
+++ src/erDiagram/shapes/entities/AssociativeEntityShape.js
@@ -0,0 +1,32 @@
+import { Shape } from "../Shape.js";
+
+export class AssociativeEntityShape extends Shape {
+    constructor(ctx, x, y, width, height) {
+        super(ctx, x, y, width, height);
+    }
+
+    // Check if the mouse is inside the rectangle
+    isInside(mouseX, mouseY) {
+        return mouseX >= this.x && mouseX <= this.x + this.width && mouseY >= this.y && mouseY <= this.y + this.height;
+    }
+
+    draw(text, backgroundColor, textColor) {
+        this.ctx.fillStyle = backgroundColor;
+        this.ctx.fillRect(this.x, this.y, this.width, this.height); // Draw the rectangle
+
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.x, this.y + this.height / 2);
+        this.ctx.lineTo(this.x + this.width / 2, this.y);
+        this.ctx.lineTo(this.x + this.width, this.y + this.height / 2);
+        this.ctx.lineTo(this.x + this.width / 2, this.y + this.height);
+        this.ctx.lineTo(this.x, this.y + this.height / 2);
+        this.ctx.closePath();
+        this.ctx.stroke();
+
+        this.ctx.strokeRect(this.x, this.y, this.width, this.height);
+
+        // Draw the text inside the rectangle
+        this.ctx.fillStyle = textColor;
+        this.ctx.fillText(text, this.x + this.width / 2, this.y + this.height / 2);
+    }
+}
