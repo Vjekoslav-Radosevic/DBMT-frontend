095d5e930cc5b3c506da9906b5aa049e00b64a34
Vjekoslav-Radosevic <vjekoslav.radosevic@gmail.com>
2025-03-04 23:27:31 +0100

    update App component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,36 @@
 <template>
-    <RouterView />
+    <router-view />
 </template>
+
+<style>
+html,
+body {
+    width: 100vw;
+    height: 100vh;
+}
+
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+#app {
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+    font-family: "DM Sans", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 500;
+    font-size: 15px;
+}
+
+input {
+    background-color: rgb(234, 234, 234);
+    border: none;
+    border-bottom: 2px solid green;
+    padding: 8px;
+    width: 100%;
+    font-size: 15px;
+}
+</style>
