20f387a1390faac753804b8a723743296e8a03d1
Vjekoslav-Radosevic <vjekoslav.radosevic@gmail.com>
2025-03-04 23:24:20 +0100

    add Relationship connection model

---
+++ src/erDiagram/models/connections/RelationshipConnection.js
@@ -0,0 +1,20 @@
+import { RelationshipConnectionShape } from "../../shapes/connections/RelationshipConnectionShape.js";
+import { Connection } from "../Connection.js";
+
+export class RelationshipConnection extends Connection {
+    constructor(ctx, element1, element2) {
+        super(ctx, element1, element2); // element1 is always Relationship
+        this.connectionShape = new RelationshipConnectionShape(ctx);
+    }
+
+    draw() {
+        let entityNearestPoint = this.element2.getNearestPoint(this.element1.getCenter());
+        let relationshipNearestPoint = this.element1.getNearestEndpoint(entityNearestPoint.x, entityNearestPoint.y);
+
+        let entity = Object.values(this.element1.entities).find((value) => value.entity === this.element2);
+
+        if (!entity) return; // if connection is not updated yet
+
+        this.connectionShape.draw(entityNearestPoint, relationshipNearestPoint, entity);
+    }
+}
