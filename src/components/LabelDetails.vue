fc5c64f0cecc5683e010b69419f2464349167303
Vjekoslav-Radosevic <vjekoslav.radosevic@gmail.com>
2025-03-06 16:29:11 +0100

    update style and placeholder for label textarea

--- src/components/LabelDetails.vue
+++ src/components/LabelDetails.vue
@@ -1,9 +1,27 @@
 <template>
-    <textarea v-model="element.text"></textarea>
+    <textarea v-model="element.text" :placeholder="placeholderText" class="label-textarea"></textarea>
 </template>
 
 <script>
 export default {
     props: ["element"],
+    data() {
+        return {
+            placeholderText: "Label content...",
+        };
+    },
 };
 </script>
+
+<style scoped>
+.label-textarea {
+    width: 90%;
+    height: 200px;
+    margin: 10px;
+    padding: 10px;
+    font-family: "DM Sans", sans-serif;
+    font-size: 15px;
+    border: 1px solid green;
+    border-radius: 4px;
+}
+</style>
