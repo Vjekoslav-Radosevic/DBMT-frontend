4312ac78da41a73db1d82b6846df451baca0f688
Vjekoslav-Radosevic <vjekoslav.radosevic@gmail.com>
2025-03-06 16:29:51 +0100

    update width and height of label according to it's text

--- src/erDiagram/shapes/LabelShape.js
+++ src/erDiagram/shapes/LabelShape.js
@@ -12,21 +12,30 @@ export class LabelShape extends Shape {
     draw(text, backgroundColor, textColor, borderColor) {
         this.ctx.save();
 
-        const lineHeight = 15;
+        const textLineHeight = 15; // 1px bigger than text alone
         const textPadding = 10;
 
-        const textLines = this.formTextLines(text, textPadding);
-        const textHeight = textLines.length * lineHeight + 2 * textPadding;
+        const { textLines, maxTextLineWidth } = this.formTextLines(text, textPadding);
 
-        this.drawLabel(backgroundColor, borderColor, textHeight);
+        // set wider width if text requires it
+        if (this.width < maxTextLineWidth + 2 * textPadding) {
+            this.width = maxTextLineWidth + 2 * textPadding;
+        }
+
+        //set higher height if text requires it
+        if (this.height < textLines.length * textLineHeight + 2 * textPadding) {
+            this.height = textLines.length * textLineHeight + 2 * textPadding;
+        }
+
+        this.drawLabel(backgroundColor, borderColor);
 
         this.ctx.fillStyle = textColor;
-        this.drawText(textLines, textPadding, lineHeight);
+        this.drawText(textLines, textPadding, textLineHeight);
 
         this.ctx.restore();
     }
 
-    drawLabel(backgroundColor, borderColor, textHeight) {
+    drawLabel(backgroundColor, borderColor) {
         this.ctx.beginPath();
         this.ctx.moveTo(this.x, this.y);
         this.ctx.lineTo(this.x + this.width - 10, this.y);
@@ -34,8 +43,8 @@ export class LabelShape extends Shape {
         this.ctx.lineTo(this.x + this.width, this.y + 10);
         this.ctx.moveTo(this.x + this.width - 10, this.y);
         this.ctx.lineTo(this.x + this.width, this.y + 10);
-        this.ctx.lineTo(this.x + this.width, this.y + textHeight);
-        this.ctx.lineTo(this.x, this.y + textHeight);
+        this.ctx.lineTo(this.x + this.width, this.y + this.height);
+        this.ctx.lineTo(this.x, this.y + this.height);
         this.ctx.lineTo(this.x, this.y);
         this.ctx.closePath();
 
@@ -48,30 +57,36 @@ export class LabelShape extends Shape {
 
     formTextLines(text, textPadding) {
         let words = text.split(" ");
-        let line = "";
-        let lines = [];
+        let textLine = "";
+        let textLines = [];
+
+        let maxTextLineWidth = 0;
 
         for (let i = 0; i < words.length; i++) {
-            let testLine = line + words[i] + " ";
+            let testLine = textLine + words[i] + " ";
             let testWidth = this.ctx.measureText(testLine).width;
             if (testWidth > this.width - 2 * textPadding && i > 0) {
-                lines.push(line);
-                line = words[i] + " ";
+                // set new maxTextLineWidth if necessary
+                let textLineWidth = this.ctx.measureText(textLine).width;
+                if (maxTextLineWidth < textLineWidth) maxTextLineWidth = textLineWidth;
+
+                textLines.push(textLine);
+                textLine = words[i] + " ";
             } else {
-                line = testLine;
+                textLine = testLine;
             }
         }
-        lines.push(line); // Add the last line
+        textLines.push(textLine); // Add the last line
 
-        return lines;
+        return { textLines, maxTextLineWidth };
     }
 
-    drawText(textLines, textPadding, lineHeight) {
+    drawText(textLines, textPadding, textLineHeight) {
         this.ctx.textAlign = "left";
         this.ctx.textBaseline = "hanging";
 
         for (let i = 0; i < textLines.length; i++) {
-            this.ctx.fillText(textLines[i], this.x + textPadding, this.y + textPadding + i * lineHeight);
+            this.ctx.fillText(textLines[i], this.x + textPadding, this.y + textPadding + i * textLineHeight);
         }
     }
 }
