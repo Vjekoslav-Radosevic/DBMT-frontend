51ab8af2cd888bd5f4317be4406e4739ea7ed139
Vjekoslav-Radosevic <vjekoslav.radosevic@gmail.com>
2025-03-08 14:23:43 +0100

    add entityTextProp

--- src/erDiagram/models/connections/RelationshipConnection.js
+++ src/erDiagram/models/connections/RelationshipConnection.js
@@ -2,9 +2,10 @@ import { RelationshipConnectionShape } from "../../shapes/connections/Relationsh
 import { Connection } from "../Connection.js";
 
 export class RelationshipConnection extends Connection {
-    constructor(ctx, element1, element2) {
+    constructor(ctx, element1, element2, entityText) {
         super(ctx, element1, element2); // element1 is always Relationship
 
+        this.entityText = entityText;
         this.connectionShape = new RelationshipConnectionShape(ctx);
     }
 
@@ -13,7 +14,9 @@ export class RelationshipConnection extends Connection {
         const relationshipNearestPoint = this.element1.getNearestEndpoint(entityCenter.x, entityCenter.y);
         const entityNearestPoint = this.element2.getNearestPoint(relationshipNearestPoint);
 
-        let entity = Object.values(this.element1.entities).find((value) => value.entity === this.element2);
+        let entity = Object.values(this.element1.entities).find(
+            (value) => value.entity === this.element2 && value.text === this.entityText,
+        );
 
         if (!entity) return; // if relationship's entities property is not updated yet
 
