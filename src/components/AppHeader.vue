6a9fea018ba25c1ad9fae34da4c08aee140e713f
Vjekoslav-Radosevic <vjekoslav.radosevic@gmail.com>
2025-03-09 12:39:39 +0100

    change component name

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="header">
+        <div class="left">
+            <div class="header-button">ER Diagram</div>
+            <div class="header-button">Home</div>
+            <div class="header-button">Workspace</div>
+        </div>
+        <div class="right">
+            <div class="header-button">About</div>
+            <div class="header-button">Profile</div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.header {
+    width: 100%;
+    height: 60%;
+    background-color: rgb(185, 185, 185);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.header-button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0px 25px;
+    padding-bottom: 3px;
+    font-size: 18px;
+}
+
+.header-button:hover {
+    cursor: pointer;
+    border-bottom: 2px solid black;
+}
+
+.left,
+.right {
+    display: flex;
+}
+
+.left {
+    justify-content: flex-start;
+}
+
+.right {
+    justify-content: flex-end;
+}
+</style>
