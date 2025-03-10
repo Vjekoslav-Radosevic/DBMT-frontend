d45f5685f9c28363499ba5af14f83a73ab588bda
Vjekoslav-Radosevic <vjekoslav.radosevic@gmail.com>
2025-03-10 20:13:06 +0100

    add vite-plugin-pwa config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,41 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue(), vueDevTools()],
+    plugins: [
+        vue(),
+        vueDevTools(),
+        VitePWA({
+            strategies: "generateSW", // default, vite-plugin-pwa generates sw.js
+            registerType: "prompt", // default, will prompt user before reloading
+            workbox: {
+                globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+            },
+            manifest: {
+                name: "Database Modeling Tool",
+                short_name: "DB Modeling Tool",
+                description: "Web application for creating relational and entity-relationship database models",
+                theme_color: "#ffffff",
+                background_color: "#ffffff",
+                display: "standalone",
+                icons: [
+                    {
+                        src: "pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
+        }),
+    ],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
