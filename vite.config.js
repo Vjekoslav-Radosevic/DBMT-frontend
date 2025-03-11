638427d3cb83708180a9b0f49a9c630737573df7
Vjekoslav-Radosevic <vjekoslav.radosevic@gmail.com>
2025-03-11 12:39:38 +0100

    update manifest icons

--- vite.config.js
+++ vite.config.js
@@ -27,6 +27,11 @@ export default defineConfig({
                 display: "standalone",
                 icons: [
                     {
+                        src: "pwa-64x64.png",
+                        sizes: "64x64",
+                        type: "image/png",
+                    },
+                    {
                         src: "pwa-192x192.png",
                         sizes: "192x192",
                         type: "image/png",
@@ -35,6 +40,13 @@ export default defineConfig({
                         src: "pwa-512x512.png",
                         sizes: "512x512",
                         type: "image/png",
+                        purpose: "any",
+                    },
+                    {
+                        src: "maskable-icon-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable",
                     },
                 ],
             },
