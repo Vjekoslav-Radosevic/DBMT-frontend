5a71bdce8be2d9378fbcb29ed033513f5bd7efe6
Vjekoslav-Radosevic <vjekoslav.radosevic@gmail.com>
2025-03-11 14:18:20 +0100

    remove registerSW function

--- src/main.js
+++ src/main.js
@@ -3,19 +3,6 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import eventBus from "./plugins/event-bus";
-import { registerSW } from "virtual:pwa-register";
-
-const updateSW = registerSW({
-    onNeedRefresh() {
-        const userConfirmed = confirm("A new version is available. Reload now?");
-        if (userConfirmed) {
-            updateSW();
-        }
-    },
-    onOfflineReady() {
-        alert("The app is ready to work offline!");
-    },
-});
 
 const app = createApp(App);
 
