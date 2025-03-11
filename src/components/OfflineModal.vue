4362b2ed45d709a8908f071a59d789899d165c35
Vjekoslav-Radosevic <vjekoslav.radosevic@gmail.com>
2025-03-11 14:20:29 +0100

    add OfflineModal component

---
+++ src/components/OfflineModal.vue
@@ -0,0 +1,51 @@
+<template>
+    <div v-if="visible" class="modal">
+        <p>Ready to work offline!</p>
+        <X class="offline-modal-x" @click="closeModal" />
+    </div>
+</template>
+
+<script>
+import { X } from "lucide-vue-next";
+export default {
+    name: "OfflineModal",
+    components: { X },
+    data() {
+        return {
+            visible: true,
+        };
+    },
+    methods: {
+        showModal() {
+            this.visible = true;
+        },
+        closeModal() {
+            this.visible = false;
+        },
+    },
+};
+</script>
+
+<style scoped>
+.modal {
+    position: absolute;
+    bottom: 12px;
+    left: 12px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background: rgb(185, 185, 185);
+    padding: 10px;
+    border-radius: 5px;
+}
+
+.offline-modal-x {
+    margin-left: 10px;
+}
+
+.offline-modal-x:hover {
+    cursor: pointer;
+}
+</style>
