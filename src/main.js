dbff2c1a81c3fad778d2e6765c28570dea6a32b1
Vjekoslav-Radosevic <vjekoslav.radosevic@gmail.com>
2025-03-12 11:46:02 +0100

    import global css

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import eventBus from "./plugins/event-bus";
+import "./assets/styles.scss";
 
 const app = createApp(App);
 
