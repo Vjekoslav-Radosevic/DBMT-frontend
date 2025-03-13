666b996787ae4cc098464520c0ba017009bd6be3
Vjekoslav-Radosevic <vjekoslav.radosevic@gmail.com>
2025-03-13 17:11:48 +0100

    add AppHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
+    <AppHeader></AppHeader>
     <router-view />
     <OfflineModal ref="offlineModalRef" />
     <NewVersionModal ref="newVersionModalRef" @reload-page="handleReload" />
 </template>
 
 <script>
+import AppHeader from "@/components/AppHeader.vue";
 import OfflineModal from "@/components/OfflineModal.vue";
 import NewVersionModal from "@/components/NewVersionModal.vue";
 import { registerSW } from "virtual:pwa-register";
 
 export default {
     name: "App",
-    components: { OfflineModal, NewVersionModal },
+    components: { AppHeader, OfflineModal, NewVersionModal },
     data() {
         return {
             updateSW: null,
