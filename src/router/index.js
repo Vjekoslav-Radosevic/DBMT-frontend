4f34be4ccbf0a4bdae0b41634b5eefa4e1ced452
Vjekoslav-Radosevic <vjekoslav.radosevic@gmail.com>
2025-03-13 17:11:15 +0100

    add diagram and catch-all routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import DiagramView from "../views/DiagramView.vue";
+import NotFoundView from "../views/NotFoundView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
             path: "/",
             name: "home",
             component: HomeView,
+        },
+        {
+            path: "/diagram",
+            name: "diagram",
+            component: DiagramView,
+        },
+        {
+            path: "/:catchAll(.*)",
+            name: "notFound",
+            component: NotFoundView,
         },
     ],
 });
