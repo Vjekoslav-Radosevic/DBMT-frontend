ed64149c0486fdbe370f281d21e55390de617c24
Vjekoslav-Radosevic <vjekoslav.radosevic@gmail.com>
2025-03-17 20:27:06 +0100

    update gsi login handler

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -8,7 +8,12 @@
 
 <script>
 export default {
-    name: "NewVersionModal",
+    name: "SignUp",
+    data() {
+        return {
+            backendUrl: "http://localhost:8080/api/users",
+        };
+    },
     mounted() {
         const gClientId = "592191942585-v6496s1r4olc6olr0ht938gk6ekg9f9b.apps.googleusercontent.com";
         window.google.accounts.id.initialize({
@@ -28,8 +33,49 @@ export default {
                 this.$refs.dialogRef.close();
             }
         },
-        handleCredentialResponse(response) {
-            console.log(response);
+        async handleCredentialResponse(response) {
+            const user = this.decodeJwtResponse(response.credential);
+            const requestBody = {
+                email: user.email,
+                familyName: user.family_name,
+                givenName: user.given_name,
+                picture: user.picture,
+            };
+
+            console.log(requestBody);
+
+            // try {
+            //     const response = await fetch(this.backendUrl, {
+            //         method: "POST",
+            //         headers: {
+            //             "Content-Type": "application/json",
+            //         },
+            //         body: JSON.stringify(requestBody),
+            //     });
+
+            //     if (response.ok) {
+            //         const responseData = await response.text(); // Convert response to JSON
+            //         console.log("JWT Token:", responseData);
+            //     } else {
+            //         console.error("Request failed with status:", response.status);
+            //     }
+            // } catch (error) {
+            //     console.error("Error occurred:", error);
+            // }
+        },
+        decodeJwtResponse(token) {
+            let base64Url = token.split(".")[1];
+            let base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+            let jsonPayload = decodeURIComponent(
+                atob(base64)
+                    .split("")
+                    .map(function (c) {
+                        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+                    })
+                    .join(""),
+            );
+
+            return JSON.parse(jsonPayload);
         },
     },
 };
